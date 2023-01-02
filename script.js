47358f0ffb9502dd8d8211d78a6c34ac3536d553
unknown <rik_1991@hotmail.com>
2023-01-02 12:15:28 +0100

    WIP functionality added.

--- script.js
+++ script.js
@@ -22,5 +22,59 @@ function operate(operator, num1, num2) {
         case 'multiply': result = multiply(num1, num2); break;
         case 'divide': result = divide(num1, num2); break;
     }
-    return result;
+    currentCalculatedValue = result;
+    displayBox.innerHTML = result;
 }
+
+let currentSelectedNum = '';
+let currentCalculatedValue = '';
+let num1;
+let num2;
+let operator;
+let displayBox = document.getElementById('resultVal');
+
+// selects all digit buttons
+let digits = document.querySelectorAll('#digits button');
+// select all operator buttosn
+let operators = document.querySelectorAll('#operators button');
+
+// add eventListeners to the digits to display them when they are clicked
+digits.forEach(digitButton => {
+    digitButton.addEventListener(
+        'click', function() {
+            currentSelectedNum += digitButton.value;
+            displayBox.innerHTML = currentSelectedNum;
+            console.log('click: ' + digitButton.value);
+})});
+
+// add eventListeners to the operators
+operators.forEach(operatorButton => {
+    operatorButton.addEventListener('click', function() {
+        operator = operatorButton.value;
+        num1 = currentSelectedNum;
+        currentSelectedNum = '';
+        console.log('operator clicked: ' + operator);
+    })
+})
+
+
+// EQUALS
+document.querySelector('#equals button').addEventListener('click', function() {
+    num2 = currentSelectedNum;
+    console.log('num1:' + num1);
+    console.log('num2:' + num2);
+    console.log('operator:' + operator);
+    // call operate function with operator, num1 and num2
+    operate(operator, +num1, +num2);
+})
+
+// clear
+document.getElementById('clearBtn').addEventListener('click', function() {
+    currentSelectedNum = '';
+    num1 = '';
+    num2 = '';
+    operator = '';
+    displayValue = '';
+    displayBox.innerHTML = displayValue;
+    currentCalculatedValue = '';
+})
