2b8a73e239766f8483bdcd84ee0ef1c8654cbc3a
unknown <rik_1991@hotmail.com>
2023-01-04 21:03:45 +0100

    recent changes, still WIP. Added css.

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ function divide(num1, num2) {
 }
 
 function operate(operator, num1, num2) {
+    console.log('in operate: ')
+    console.log('operator: ' + operator)
+    console.log('num1: ' + num1)
+    console.log('num2: ' + num2)
     let result = 0;
     switch (operator) {
         case 'add': result = add(num1, num2); break;
@@ -34,42 +38,42 @@ let operator;
 let displayBox = document.getElementById('resultVal');
 
 // selects all digit buttons
-let digits = document.querySelectorAll('#digits button');
-// select all operator buttosn
-let operators = document.querySelectorAll('#operators button');
+let digits = document.querySelectorAll('.bttn.input');
+// select all operator buttons
+let operators = document.querySelectorAll('.bttn.operator');
 
-// add eventListeners to the digits to display them when they are clicked
+// add eventListeners to the digits to display them when they are clicked and handle functionality
 digits.forEach(digitButton => {
     digitButton.addEventListener(
         'click', function() {
+            console.log('button clicked')
+            console.log('clicked: ' + digitButton.value);
             currentSelectedNum += digitButton.value;
             displayBox.innerHTML = currentSelectedNum;
-            console.log('click: ' + digitButton.value);
 })});
 
 // add eventListeners to the operators
 operators.forEach(operatorButton => {
     operatorButton.addEventListener('click', function() {
+        console.log('clicked operator: ' + operatorButton.value);
         operator = operatorButton.value;
-        num1 = currentSelectedNum;
+        if (currentCalculatedValue) {
+            num1 = currentCalculatedValue;
+        } else {
+            num1 = currentSelectedNum;
+        }
         currentSelectedNum = '';
-        console.log('operator clicked: ' + operator);
     })
 })
 
-
 // EQUALS
-document.querySelector('#equals button').addEventListener('click', function() {
+document.getElementById('equalsBttn').addEventListener('click', function() {
     num2 = currentSelectedNum;
-    console.log('num1:' + num1);
-    console.log('num2:' + num2);
-    console.log('operator:' + operator);
-    // call operate function with operator, num1 and num2
     operate(operator, +num1, +num2);
 })
 
 // clear
-document.getElementById('clearBtn').addEventListener('click', function() {
+document.getElementById('clearBttn').addEventListener('click', function() {
     currentSelectedNum = '';
     num1 = '';
     num2 = '';
